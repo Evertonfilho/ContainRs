2a9f1bea91cf851f211093cd547900ba6b73f297
evertonfilho <evertonfilho0513@gmail.com>
2025-05-31 01:19:59 -0300

    Projeto de cadastro de clientes

---
+++ ContainRs.Application/UseCases/ConsultarClientes.cs
@@ -0,0 +1,25 @@
+﻿using ContainRs.Application.Repositories;
+using ContainRs.Domain.Models;
+
+namespace ContainRs.Application.UseCases;
+public class ConsultarClientes
+{
+    private readonly IClienteRepository _repository;
+    public ConsultarClientes(UnidadeFederativa? estado, IClienteRepository repository)
+    {
+        Estado = estado;
+        _repository = repository;
+    }
+
+    public UnidadeFederativa? Estado { get; }
+
+    public async Task<IEnumerable<Cliente>> ExecutarAsync()
+    {
+        if (Estado is not null)
+        {
+            return await _repository.GetAsync(c => c.Estado == Estado);
+        }
+        return await _repository.GetAsync();
+    }
+
+}
